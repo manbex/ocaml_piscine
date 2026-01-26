b02c5ebc5fe36fc0379ab41d51f107e1e9f9ec5b
Max <max@manbex.fr>
2026-01-26 13:36:25 +0100

    bettre syntax

--- basic_syntax_and_semantics/ex00/ft_test_sign.ml
+++ basic_syntax_and_semantics/ex00/ft_test_sign.ml
@@ -1,9 +1,9 @@
+let compare n =
+	if n < 0
+	then "negative"
+	else "positive"
+
 let ft_test_sign n =
-	let compare n =
-		if n < 0
-		then "negative"
-		else "positive"
-	in
 	print_endline (compare n)
 
 
